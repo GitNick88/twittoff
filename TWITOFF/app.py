ae9c8c67ebb4f8334f54c686670498b584e150a0
Nick Cole <tcnick12@gmail.com>
2020-02-24 17:48:24 -0500

    'Update'

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,13 +1,20 @@
-''' Code for our app'''
+''' Code for our app '''
 
 # import, make app, make route
 
 from flask import Flask
+from .models import DB
 
 # Make our app factory
 
 def create_app():
     app = Flask(__name__)
+
+    # add config for database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    
+    #have the database know about the app
+    DB.init_app(app)
 
     @app.route('/')
     def root():
