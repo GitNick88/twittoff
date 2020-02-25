fedab5d1a38ce1705402c45f863d4aecfaeebbba
Nick Cole <tcnick12@gmail.com>
2020-02-25 12:53:18 -0500

    made a few changes

--- TWITOFF/__init__.py
+++ TWITOFF/__init__.py
@@ -2,8 +2,5 @@
 
 from .app import create_app
 
+#Capitalize APP because it's a global variable
 APP = create_app()
-
-# if __name__ == "__main__":
-#     my_app = create_app()
-#     my_app.run(debug=True)
