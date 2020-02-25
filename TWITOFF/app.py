ba0d114e9ee4f2d2b20330d3c98fa2bc945b2c3f
Nick Cole <tcnick12@gmail.com>
2020-02-25 15:40:26 -0500

    added .env and twitter.py

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -11,15 +11,18 @@ def create_app():
     app = Flask(__name__)
 
     # add config for database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+
+    # Stop trackingmodifications on sqlalchemy confid
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     #have the database know/connect the app
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        user = User.query.all()
-        return render_template('base.html', title='Home', user=users)
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
     return app
 
 # Make route
